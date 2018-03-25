5d8d086ff9fc1abcc9a389c7febffa558988aad8
MrH0wl <32224733+MrH0wl@users.noreply.github.com>
2018-03-25 03:55:13 -0600

    Upgrade to 1.4
    In this version I wanted to start implementing the full automation of the script.

--- Cloudmare.py
+++ Cloudmare.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Cloudmare v1.0
+# Cloudmare v1.4
 # By Secmare - twitter.com/secmare 
 
+import os
 import sys
 import socket
 import colorama
 import requests
 import urllib3
+import dns
 import dns.resolver
 import dns.exception
 import argparse
 
+from dnsdumpster.DNSDumpsterAPI import DNSDumpsterAPI
 from html_similarity import similarity
 from dns.resolver import Resolver
 from colorama import Fore, Style, Back
@@ -22,147 +25,213 @@ config = {
     'http_timeout_seconds': 3,
     'response_similarity_threshold': 0.9
 }
-logo = '''\
+
+def logotype():
+	print Fore.YELLOW + '''\
   ____ _                 _ __  __                
  / ___| | ___  _   _  __| |  \/  | __ _ _ __ ___ 
 | |   | |/ _ \| | | |/ _` | |\/| |/ _` | '__/ _ \                  
 | |___| | (_) | |_| | (_| | |  | | (_| | | |  __/
  \____|_|\___/ \__,_|\__,_|_|  |_|\__,_|_|  \___|
- '''
-
-logo2 ='''
+	''' + Fore.RESET + Style.DIM + Fore.WHITE + '''
  ================================================
  ||                  Secmare                   ||
  ||            twitter.com/secmare             ||
- ||                   v1.0                     ||
+ ||                   v1.4                     ||
  ================================================
-'''
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	''' + Fore.RESET
 
-parser.add_argument("target", metavar="[domain]",help="The domain to scan.")
-parser.add_argument("-s", dest='subdomain', default="None", help="Server to compare with target.")
-parser.add_argument("-v", '--version', action='version', version='%(prog)s 1.0 | Copyright 2018 - GPL v3.0')
-args = parser.parse_args()
-hostname = args.target
-server_name = args.subdomain
+	
+def parse_error(errmsg):
+    logotype()
+    print("Usage: python " + sys.argv[0] + " [Options] use -h for help")
+    print(Fore.BLUE + "[*] " + Fore.RESET + "Error: \n" + Fore.RED + "   [-] " + Fore.RESET + errmsg)
+    exit(1)
+	
+def parse_args():
+	parser = argparse.ArgumentParser(epilog="\tExample: \r\npython " + sys.argv[0] + " [DOMAIN] -s -o output.txt")
+	parser.error = parse_error
+	parser._optionals.title = "OPTIONS"
+	parser.add_argument("-v", '--version', action='version', version='%(prog)s 1.0 | Copyright 2018 - GPL v3.0')
+	parser.add_argument("domain", metavar="[domain]",help="The domain to scan")
+	parser.add_argument("-s", "--subdomain", help="Scan for subdomain", action="store_true", default=False, required=True)
+	#parser.add_argument("-i", "--input", help="Import your list of subdomains in format .txt")
+	#parser.add_argument("-o", "--output", help="Export the data from the script")
+	return parser.parse_args()
+	
 
-print(Fore.YELLOW + logo), (Style.DIM + Fore.WHITE + logo2)
 
-sys_r = Resolver()
+def put_file():
+	line = open('vulnsubdomain.txt')
+	file = line.readline().replace("\n", "")
+	line.close
+	return file
+		
+def subdomain_tracking(domain):
+	print (Fore.YELLOW + Style.BRIGHT + "Tracking subdomains and MX records...\n")
+	ip_takes = []
+	res = DNSDumpsterAPI(False).search(domain)
+	if res['dns_records']['host']:
+		print (Fore.BLUE + "[*] " + Fore.RESET + "Subdomains:")
+		for entry in res['dns_records']['host']:
+			print (Fore.GREEN + "   [+] " + Fore.RESET + "{ip}".format(**entry) + " from: " + "{domain}".format(**entry))
+	if res['dns_records']['mx']:
+		print (Fore.BLUE + "[*] " + Fore.RESET + "MX Records:")
+		for entry in res['dns_records']['mx']:
+			print (Fore.GREEN + "   [+] " + Fore.RESET + "{ip}".format(**entry) + " from: " + "{domain}".format(**entry))
+	print(Fore.BLUE + "\n[*]" + Fore.RESET + "Enumerating misconfigured DNS subdomains:")
 
-print(Fore.YELLOW + Style.BRIGHT +"Cloudflare IP Catcher (Auto DIG)...")
+	try:
+		for entry in res['dns_records']['host']:
+			provider = str(entry['provider'])
+			ip = str(entry['ip'])
+			if "Cloudflare" not in provider and ip not in ip:
+				ip_takes.append('{ip}'.format(**entry))
+				print (Fore.GREEN + "   [+] " + Fore.RESET + "{ip}".format(**entry) + " from: " + "{domain}".format(**entry))
+		for entry in res['dns_records']['mx']:
+			provider = str(entry['provider'])
+			ip = str(entry['ip'])
+			if "Cloudflare" not in provider and ip not in ip_takes:
+				ip_takes.append('{ip}'.format(**entry))
+				print (Fore.GREEN + "   [+] " + Fore.RESET + "{ip}".format(**entry) + " from: " + "{domain}".format(**entry))
+	except:
+		print(Fore.RED + "   [-]" + Fore.RESET + " All IPs belong to the Cloudflare Network")
+		exit(1)
+	if ip_takes != None:
+		with open('vulnsubdomain.txt', 'w') as file:
+			ip_takes = map(str, ip_takes)
+			line = "\n".join(ip_takes)
+			file.write(line)
 
-domain = hostname
-port = 80
-ns = server_name
-dns = [ns]
-print(Fore.BLUE + "[*]" + Fore.RESET + " Checking if {0} are similar to {1}...".format(ns, domain))
-test1 = requests.get('http://' + domain, timeout=config['http_timeout_seconds'])
-test2 = requests.get('http://' + ns, timeout=config['http_timeout_seconds'])
-page_similarity2 = similarity(test1.text, test2.text)
-if page_similarity2 > config['response_similarity_threshold']:
-	print (((Fore.GREEN + '[+]' + Fore.RESET) + ' HTML content is %d%% structurally similar to: %s' % (round(100 *page_similarity2, 2), domain)))
-else:
-	print (((Fore.RED + '[-]' + Fore.RESET + ' Sorry, but HTML content is %d%% structurally similar to %s' % (round(100 *page_similarity2, 2), domain))))
-	print ("\n - Trying to check with IP... \n")
+		
+def first_scan():
+	print(Fore.YELLOW + Style.BRIGHT +"Cloudflare IP Catcher (Auto DIG)...\n")
+	print(Fore.BLUE + "[*]" + Fore.RESET + " Checking if {0} are similar to {1}".format(ns, domain))
+	test1 = requests.get('http://' + domain, timeout=config['http_timeout_seconds'])
+	test2 = requests.get('http://' + ns, timeout=config['http_timeout_seconds'])
+	page_similarity2 = similarity(test1.text, test2.text)
+	if page_similarity2 > config['response_similarity_threshold']:
+		print (((Fore.GREEN + '   [+]' + Fore.RESET) + ' HTML content is %d%% structurally similar to: %s' % (round(100 *page_similarity2, 2), domain)))
+	else:
+		print (((Fore.RED + '   [-]' + Fore.RESET + ' Sorry, but HTML content is %d%% structurally similar to %s' % (round(100 *page_similarity2, 2), domain))))
+		print ("\n - Trying to check with IP... \n")
+	
 
-def netcat(domain):
+
+def first_nc(domain):
 	ip = socket.gethostbyname(ns)
 	url = 'http://' + domain
 	page = requests.get(url, timeout=config['http_timeout_seconds'])
 	hncat = page.url.replace('https://www.', '').split('/')[0]
 	home = page.url.replace('https://', '').split('.')[-2]
-	home2 = home.replace('com', '').split('/')[1]
-	print ((Fore.BLUE + '[*]' + Fore.RESET + ' Testing if are the real IP...'))
+	print ((Fore.BLUE + '[*]' + Fore.RESET + ' Testing if are the real IP'))
 	data = "GET /{0} HTTP/1.1\r\nHost: {1}\r\n\r\n".format(home, hncat)
 
 	client = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-	client.connect(( ip, port ))
+	client.connect(( ip, 80 ))
 	client.sendall(data)
 	response = client.recv(2048)
 	if '200 OK' in response:
-		print ((Fore.GREEN + "[+]" + Fore.RESET) + " The Real IP is: %s." % ip)
-		sys.exit(1)
+		print ((Fore.GREEN + "   [+]" + Fore.RESET) + " The Real IP is: %s." % ip)
+		exit(1)
 	if '301' in response:
-		print ((Fore.GREEN + "[+]" + Fore.RESET) + " The Real IP is: %s." % ip)
-		sys.exit(1)
+		print ((Fore.GREEN + "   [+]" + Fore.RESET) + " The Real IP is: %s." % ip)
+		exit(1)
 	else:
-		print ((Fore.RED + "[-]" + Fore.RESET) + " %s is not the IP." %ip)
+		print ((Fore.RED + "   [-]" + Fore.RESET) + " %s is not the IP." %ip)
 		print ((Fore.BLUE + '[*]' + Fore.RESET) + ' Trying to DIG for obtain the Real IP')
-netcat(domain)
 
-try:
-	dream_dns = [item.address for server in dns for item in sys_r.query(server)]
-	dream_r = Resolver()
-	dream_r.nameservers = dream_dns	
-	answer = dream_r.query(domain, 'A')
-	for A in answer.rrset.items:
-		print (Fore.GREEN + '[+]' + Fore.RESET) + ' Possible IP:', A
-except:
-	print (Fore.RED + '[-]' + Fore.RESET) + ' IP not found.'
-	print ("\n - Exiting...")
-	sys.exit(1)
+def Checking_DNS(ns, dns):
+	try:
+		dream_dns = [item.address for server in dns for item in sys_r.query(server)]
+		dream_r = Resolver()
+		dream_r.nameservers = dream_dns	
+		answer = dream_r.query(domain, 'A')
+		for A in answer.rrset.items:
+			A
+	except:
+		print (Fore.RED + '   [-]' + Fore.RESET) + ' IP not found.'
+		print ("\n - Exiting...")
+		exit(1)
+	return A
 	
 def Checking_IP(domain):
-	#print(Fore.YELLOW + logo)
+	A = Checking_DNS(ns, dns)
+	print (Fore.GREEN + '   [+]' + Fore.RESET) + ' Possible IP:', A
 	url = 'http://' + domain
 	print((Fore.BLUE + '[*]') + Fore.RESET + ' Retrieving target homepage at: %s' % url)
 	try:
 		org_response = requests.get(url, timeout=config['http_timeout_seconds'])
 	except requests.exceptions.Timeout:
-		sys.stderr.write((Fore.RED + "[-]" + Fore.RESET) + " %s timed out after %d seconds.\n" % (url, config['http_timeout_seconds']))
+		sys.stderr.write((Fore.RED + "   [-]" + Fore.RESET) + " %s timed out after %d seconds.\n" % (url, config['http_timeout_seconds']))
 		exit(1)
 	except requests.exceptions.RequestException as e:
-		sys.stderr.write((Fore.RED + "[-]" + Fore.RESET) + " Failed to retrieve %s.\n" % url)
+		sys.stderr.write((Fore.RED + "   [-]" + Fore.RESET) + " Failed to retrieve %s.\n" % url)
 		exit(1)
 
 	if org_response.status_code != 200:
-		print (Fore.RED + '[-]' + Fore.RESET) + ' %s responded with an unexpected HTTP status code %d' % (url, org_response.status_code)
+		print (Fore.RED + '   [-]' + Fore.RESET) + ' %s responded with an unexpected HTTP status code %d' % (url, org_response.status_code)
 		exit(1)
 
 	if org_response.url != url:
 		print ((Fore.BLUE + '[*]' + Fore.RESET) + ' %s redirects to %s.' % (url, org_response.url))
-		print ('\n - Redirecting to %s.' % org_response.url)
-		print ('\n' + Fore.GREEN + "[+]" + Fore.RESET + " Request redirected successful to %s." % org_response.url)
+		print ('\n' + Fore.GREEN + "   [+]" + Fore.RESET + " Request redirected successful to %s." % org_response.url)
 	print ((Fore.BLUE + '[*]' + Fore.RESET + ' Testing if body content is the same in both websites.'))
 	
 	sec_response = requests.get('http://' + str(A), timeout=config['http_timeout_seconds'])
 	if sec_response.status_code != 200:
-		print (Fore.RED + '[-]' + Fore.RESET) + ' %s responded with an unexpected HTTP status code %d' % (url, org_response.status_code)
+		print (Fore.RED + '   [-]' + Fore.RESET) + ' %s responded with an unexpected HTTP status code %d' % (url, org_response.status_code)
 		exit(1)
 	if sec_response.text == org_response.text:
 		print ((str(A), 'HTML content identical to %s' % domain))
 	page_similarity = similarity(sec_response.text, org_response.text)
 	if page_similarity > config['response_similarity_threshold']:
-		print (((Fore.GREEN + '[+]' + Fore.RESET) + ' HTML content is %d%% structurally similar to: %s' % (round(100 *page_similarity, 2), org_response.url)))
-	else:
-		print (Fore.RED + '[-]' + Fore.RESET + ' %s is not the real IP.' % str(A))
+		print (((Fore.GREEN + '   [+]' + Fore.RESET) + ' HTML content is %d%% structurally similar to: %s' % (round(100 *page_similarity, 2), org_response.url)))
 	return org_response
 
-def netcat2(domain):
+def second_nc(domain):
+	A = Checking_DNS(ns, dns)
 	ip = str(A)
 	url = 'http://' + domain
 	page = requests.get(url, timeout=config['http_timeout_seconds'])
 	hncat = page.url.replace('https://www.', '').split('/')[0]
 	home = page.url.replace('https://', '').split('.')[-2]
-	home2 = home.replace('com', '').split('/')[1]
-	print ((Fore.BLUE + '[*]' + Fore.RESET + ' Testing if are the correct IP...'))
+	print ((Fore.BLUE + '[*]' + Fore.RESET + ' Testing if are the real IP'))
 	data = "GET /{0} HTTP/1.1\r\nHost: {1}\r\n\r\n".format(home, hncat)
 
 	client = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-	client.connect(( ip, port ))
+	client.connect(( ip, 80 ))
 	client.sendall(data)
 	response = client.recv(2048)
 	if '200 OK' in response:
-		print ((Fore.GREEN + "[+]" + Fore.RESET) + " The Real IP is: %s." % ip)
-		sys.exit(1)
+		print ((Fore.GREEN + "   [+]" + Fore.RESET) + " The Real IP is: %s." % ip)
+		exit(1)
 	if '301' in response:
-		print ((Fore.GREEN + "[+]" + Fore.RESET) + " The Real IP is: %s." % ip)
-		sys.exit(1)
+		print ((Fore.GREEN + "   [+]" + Fore.RESET) + " The Real IP is: %s." % ip)
+		exit(1)
 	else:
-		print ((Fore.RED + "[-]" + Fore.RESET) + " %s is not the IP." %ip)
-		sys.exit(1)
+		print ((Fore.RED + "   [-]" + Fore.RESET) + " %s is not the IP." %ip)
 
 if __name__=="__main__":
-	Checking_IP(domain),
-	netcat2(domain)
+	try:
+		args = parse_args()
+		sys_r = Resolver()
+		domain = args.domain
+		ns = put_file()
+		dns = [ns]
+		if args.subdomain == True:
+			logotype()
+			subdomain_tracking(domain)
+		else:
+			exit(1)
+		first_scan()
+		first_nc(domain)
+		Checking_DNS(ns, dns)
+		Checking_IP(domain)
+		second_nc(domain)
+	except KeyboardInterrupt:
+		print ("\n" * 80)
+		os.system('clear')
+		logotype()
+		print(Fore.YELLOW + " ~ Thanks to use this script! <3")
+		sys.exit(0)
--- README.md
+++ README.md
@@ -7,40 +7,59 @@ For more detail about this common misconfiguration and how Cloudmare works, send
 Here's what Cloudmare looks like in action.
 
 ```
-$ python Cloudmare.py target.site -s sub.myvulnerable.site
-
+$ python Cloudmare.py target.site -s
   ____ _                 _ __  __                
  / ___| | ___  _   _  __| |  \/  | __ _ _ __ ___ 
 | |   | |/ _ \| | | |/ _` | |\/| |/ _` | '__/ _ \                  
 | |___| | (_) | |_| | (_| | |  | | (_| | | |  __/
  \____|_|\___/ \__,_|\__,_|_|  |_|\__,_|_|  \___|
-  
+	
  ================================================
  ||                  Secmare                   ||
  ||            twitter.com/secmare             ||
- ||                   v1.0                     ||
+ ||                   v1.4                     ||
  ================================================
+	
+Tracking subdomains and MX records...
 
+[*] Subdomains:
+   [+] 104.28.22.23 from: target.site
+   [+] 104.28.22.23 from: ns1.target.site
+   [+] 123.56.45.42 from: mail2.target.site
+   [+] 104.28.22.23 from: ns2.target.site
+   [+] 104.28.22.23 from: www.database.target.site
+   [+] 104.28.22.23 from: www.forum.target.site
+   [+] 104.28.22.23 from: www.target.site
+   [+] 123.56.45.49 from: mail.target.site
+   [+] 123.56.45.49 from: vps.target.site
+[*] MX Records:
+   [+] 123.56.45.42 from: 2 mail2.target.site.
+   [+] 123.56.45.49 from: 0 mail.target.site.
+   [+] 123.56.45.49 from: 10 vps.target.site.
+
+[*]Enumerating misconfigured DNS subdomains:
+   [+] 123.56.45.42 from: 2 mail2.target.site.
+   [+] 123.56.45.49 from: 0 mail.target.site.
 Cloudflare IP Catcher (Auto DIG)...
-[*] Checking if sub.myvulnerable.site are similar to target.site...
-[-] Sorry, but HTML content is 0% structurally similar to target.site
+
+[*] Checking if 123.56.45.49 are similar to target.site
+   [-] Sorry, but HTML content is 0% structurally similar to target.site
 
  - Trying to check with IP... 
 
-[*] Testing if are the real IP...
-[-] 123.56.45.49 is not the IP.
+[*] Testing if are the real IP
+   [-] 123.56.45.49 is not the IP.
 [*] Trying to DIG for obtain the Real IP
-[+] Possible IP: 23.56.48.62
+   [+] Possible IP: 23.56.48.62
 [*] Retrieving target homepage at: http://target.site
 [*] http://target.site redirects to https://www.target.site/home.php.
 
- - Redirecting to https://www.target.site/home.php.
-
-[+] Request redirected successful to https://www.target.site/home.php.
+   [+] Request redirected successful to https://www.target.site/home.php.
 [*] Testing if body content is the same in both websites.
-[+] HTML content is 100% structurally similar to: https://www.target.site/home.php
-[*] Testing if are the correct IP...
-[+] The Real IP is: 23.56.48.62.
+   [+] HTML content is 100% structurally similar to: https://www.target.site/home.php
+[*] Testing if are the real IP
+   [+] The Real IP is: 23.56.48.62.
+
 ```
 
 (_The IP addresses in this example have been obfuscated and replaced by randomly generated IPs_)
@@ -63,25 +82,38 @@ $ pip install -r requirements.txt
 3) Run Cloudmare (see [Usage](#usage) below for more detail)
 
 ```
-$ python cloudmare.py target.site -s sub.myvulnerable.site
+$ python cloudmare.py target.site -s
 ```
 
 ## Usage
 
 ```
 $ python cloudmare.py --help
-
-usage: Cloudmare.py [-h] [-s SUBDOMAIN] [-v] [domain]
+  ____ _                 _ __  __                
+ / ___| | ___  _   _  __| |  \/  | __ _ _ __ ___ 
+| |   | |/ _ \| | | |/ _` | |\/| |/ _` | '__/ _ \                  
+| |___| | (_) | |_| | (_| | |  | | (_| | | |  __/
+ \____|_|\___/ \__,_|\__,_|_|  |_|\__,_|_|  \___|
+	
+ ================================================
+ ||                  Secmare                   ||
+ ||            twitter.com/secmare             ||
+ ||                   v1.4                     ||
+ ================================================
+	
+usage: Cloudmare.py [-h] [-v] -s [domain]
 
 positional arguments:
-  [domain]       The domain to scan.
+  [domain]         The domain to scan
 
-optional arguments:
-  -h, --help     show this help message and exit
-  -s SUBDOMAIN   Server to compare with target. (default: None)
-  -v, --version  show program's version number and exit
+OPTIONS:
+  -h, --help       show this help message and exit
+  -v, --version    show program's version number and exit
+  -s, --subdomain  Scan for subdomain
+
+Example: python Cloudmare.py [DOMAIN] -s
 ```
 
 ## Compatibility
 
-Tested on Python 2.7 in Kali Linux. Feel free to [open an issue](https://github.com/MrH0wl/Cloudmare/issues/new) if you have bug reports or questions.
+Tested on Python 2.7 in Kali Linux. Feel free to [open an issue] if you have bug reports or questions. If you want to collaborate, you're welcome.

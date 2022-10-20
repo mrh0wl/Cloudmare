b0a9abc99d4085dffa9f10583f5bc76bd291ec9d
mrh0wl <h0wl.jacket@gmail.com>
2022-10-19 23:14:28 -0600

    Fixing minor bugs in netcat()

--- lib/tools/netcat.py
+++ lib/tools/netcat.py
@@ -16,12 +16,24 @@ from .ispcheck import ISPCheck
 urllib3.disable_warnings()
 
 
+def simCheck(data, page, ip, domain):
+    sim = similarity(data.text, page.text)
+    if sim > config['response_similarity_threshold']:
+        print(tab + good + 'The connection has %d%% similarity to: %s' % (round(100 * sim, 2), domain))
+        print(tab + good + '%s is the real IP' % ip)
+        quest(question='\n' + warn + 'IP found. Do yo want to stop tests?',
+              doY='sys.exit()', doN="pass")
+    else:
+        print(tab + bad + 'The connect has %d%% similarity to: %s' % (round(100 * sim, 2), domain))
+        print(tab + bad + "%s is not the IP" % ip)
+
+
 def netcat(domain, host, ignoreRedir, userAgent, randomAgent, header, count):
     headers = dict(x.replace(' ', '').split(':') for x in header.split(',')) if header is not None else {}
     headers.update({'User-agent': random.choice(open("data/txt/random_agents.txt").readlines()
                                                 ).rstrip("\n")}) if randomAgent is True else ''
     headers.update({'User-agent': userAgent}) if userAgent is not None else ''
-    schemas = ['http://', 'https://']
+
     A = DNSLookup(domain, host)
     question = None
     try:
@@ -32,39 +44,43 @@ def netcat(domain, host, ignoreRedir, userAgent, randomAgent, header, count):
             return
         print(info + 'Connecting %s using as Host Header: %s' % (ip, domain))
         count += 1
-        for i in range(len(schemas)):
-            page = requests.get(schemas[i] + domain, timeout=config['http_timeout_seconds'])
-            hncat = page.url.replace(schemas[i], '').split('/')[0]
-            headers.update(host=hncat)
-            data = requests.get(schemas[i] + ip, headers=headers,
-                                timeout=config['http_timeout_seconds'], allow_redirects=False, verify=False)
-            if data.status_code in [301, 302]:
-                print(tab + info + "%s redirects to: %s" % (schemas[i] + ip, data.headers['Location']))
-                question = ignoreRedir if ignoreRedir is not True else input(
-                    tab + info + 'Do yo want to redirect? y/n: ')
-                redir = (True if question in ['y', 'yes', 'ye']
-                         else ignoreRedir if ignoreRedir is not True
-                         else False)
-                data = requests.get(
-                    schemas[i] + ip,
-                    headers=headers,
-                    timeout=config['http_timeout_seconds'],
-                    allow_redirects=redir, verify=False
-                )
-            if data.status_code == 200:
-                count += 1
-                sim = similarity(data.text, page.text)
-                if sim > config['response_similarity_threshold']:
-                    print(tab + good + 'The connection has %d%% similarity to: %s' % (round(100 * sim, 2), domain))
-                    print(tab + good + '%s is the real IP' % ip)
-                    quest(question='\n' + warn + 'IP found. Do yo want to stop tests?',
-                          doY='sys.exit()', doN="pass")
-                else:
-                    print(tab + bad + 'The connect has %d%% similarity to: %s' % (round(100 * sim, 2), domain))
-                    print(tab + bad + "%s is not the IP" % ip)
-            else:
-                print(tab + bad + 'Unexpected status code [%s] occurred at: %s' %
-                      (data.status_code, schemas[i] + ip))
+        page = requests.get('http://' + domain, timeout=config['http_timeout_seconds'])
+        hncat = page.url.replace('http://', '').split('/')[0]
+        headers.update(host=hncat)
+        data = requests.get('http://' + ip, headers=headers,
+                            timeout=config['http_timeout_seconds'], allow_redirects=False, verify=False)
+        if data.status_code in [301, 302]:
+            print(tab + info + "%s redirects to: %s" % ('http://' + ip, data.headers['Location']))
+            question = (ignoreRedir if ignoreRedir is not True
+                        else quest(
+                            question=f'{tab}{info}Do you want to redirect?',
+                            doY='True',
+                            doN='False',
+                            **{'return': True}
+                            )
+                        )
+            data = requests.get(
+                'http://' + ip,
+                headers=headers,
+                timeout=config['http_timeout_seconds'],
+                allow_redirects=question, verify=False
+            )
+        if data.status_code == 200:
+            count += 1
+        else:
+            print(tab + bad + 'Unexpected status code [%s] occurred at: %s' %
+                  (data.status_code, data.url))
+            question = quest(
+                question=f'{tab}{warn}Do you want to force the connection anyway?',
+                doY='True',
+                doN='False',
+                **{'return': True}
+            )
+            if not question:
+                print(f'{tab}{warn}The process cannot be completed')
+                return
+
+        simCheck(data, page, ip, domain)
 
     except Exception as e:
         if question in ['y', 'yes', 'ye']:
